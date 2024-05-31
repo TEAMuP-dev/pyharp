d06ca2a6c1a2b5281c09e281d10edd69cbb889b4
Frank Cwitkowitz <cwitkowitz@gmail.com>
2024-05-30 21:56:38 -0400

    Reorganized, updated, and simplified README.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyharp',
     version='0.1.0',
-    url='https://github.com/audacitorch/pyharp',
+    url='https://github.com/TEAMuP-dev/pyharp',
     author='Hugo Flores García, Christos Benetatos, Patrick O\'Reilly and Aldo Aguilar',
     author_email='hugofloresgarcia@u.northwestern.edu',
     description='',
