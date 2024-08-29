7f8c1e332d83574fca668f4c150471d1779e068e
Frank Cwitkowitz <cwitkowitz@gmail.com>
2024-08-29 14:33:07 -0400

    Removed explicit y field from output label classes infavor of doing the conversion on the HARP side.

--- pyharp/labels.py
+++ pyharp/labels.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List, Union, Dict
 
+
 __all__ = [
     'OutputLabel',
     'AudioLabel',
@@ -18,56 +19,34 @@ class OutputLabel:
     description: str = None
 
     def __post_init__(self):
-        self.y = None
-
-    def set_y(self, y):
-        self.y = y
+        self.label_type = self.__class__.__name__
 
 
 @dataclass
 class AudioLabel(OutputLabel):
     amplitude: float = 0.0
 
-    def __post_init__(self):
-        super().__post_init__()
-
-        self.y = (self.amplitude + 1) / 2
-        self.label_type = self.__class__.__name__
-
 
 @dataclass
 class SpectrogramLabel(OutputLabel):
     frequency: float = 440.0
-
-    def __post_init__(self):
-        super().__post_init__()
-
-        # TODO - not sure what to do here yet
-        self.label_type = self.__class__.__name__
 
 
 @dataclass
 class MidiLabel(OutputLabel):
     pitch: float = 69
 
-    def __post_init__(self):
-        super().__post_init__()
-
-        self.y = self.pitch / 128
-        self.label_type = self.__class__.__name__
 
 LabelUnion = Union[AudioLabel, SpectrogramLabel, MidiLabel, OutputLabel]
 
 @dataclass
 class LabelList:
-
     meta: Dict[str, str] = field(default_factory = dict)
-
     labels: List[LabelUnion] = field(default_factory = list)
 
     def __post_init__(self):
-        # in a similar way to gradio components
-        # e.g. gr.File has meta._type = "gradio.FileData"
+        # Add meta._type to match Gradio components
+        # (e.g. for gr.File meta._type = "gradio.FileData")
         self.meta = {
             "_type": f"pyharp.{self.__class__.__name__}"
         }
