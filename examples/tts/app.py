9ee21d261cbdc59f61944401797137105fd2c21d
Nathan Pruyne <nathan.pruyne@gmail.com>
2024-12-20 02:02:47 -0600

    Add no-input TTS example

---
+++ examples/tts/app.py
@@ -0,0 +1,64 @@
+from pyharp import *
+
+import gradio as gr
+import torchaudio
+import torch
+from typing import Tuple
+
+from gtts import gTTS
+
+# Create a ModelCard
+model_card = ModelCard(
+    name="Text to speech",
+    description="Generate speech from input text.",
+    author="np",
+    tags=["example", "tts"],
+)
+
+# Define the process function
+@torch.inference_mode()
+def process_fn(input_text: str) -> Tuple[str, list]:
+
+    tts_out = gTTS(text=input_text, lang="en", slow=False)
+
+    output_path = "_outputs/output.wav"
+    tts_out.save(output_path)
+
+    # No output labels
+    output_labels = LabelList()
+
+    return output_path, output_labels
+
+# Build Gradio endpoint
+with gr.Blocks() as demo:
+    # Define Gradio Components
+    input_components = [
+        # gr.Audio(
+        #     type='filepath',
+        #     label='Output Audio'
+        # ),
+        gr.Textbox(
+            label="Text to process"
+        )
+    ]
+    output_components = [
+        gr.Audio(
+            type='filepath',
+            label='Output Audio'
+        ),
+        # gr.File(
+        #     type='filepath',
+        #     label="Output Midi",
+        #     file_types=[".mid", ".midi"]
+        # ),
+        gr.JSON(label="Output Labels")
+    ]
+
+    app = build_endpoint(
+                model_card=model_card,
+                input_components=input_components,
+                output_components=output_components,
+                process_fn=process_fn)
+
+demo.queue()
+demo.launch(share=True, show_error=True)
