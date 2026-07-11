80025dd33ff3c651a7308d7f416b9b581df15a55
Frank Cwitkowitz <cwitkowitz@gmail.com>
2026-07-11 10:29:19 -0400

    Bumped version / copyright, and updated authors in setup.py.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyharp',
-    version='0.3.0',
+    version='0.3.1',
     url='https://github.com/TEAMuP-dev/pyharp',
-    author='Frank Cwitkowitz, Christodoulos Benetatos, Hugo Flores García, Patrick O\'Reilly, Nathan Pruyne, and Aldo Aguilar',
+    author='TEAMuP',
     author_email='fcwitkow@ur.rochester.edu',
     description='',
     packages=find_packages(),
